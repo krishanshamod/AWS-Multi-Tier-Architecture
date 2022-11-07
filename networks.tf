67a854e374bb5cf154a5cc666e134decea2305df
Krishan Shamod <krishanshamod@gmail.com>
2022-11-07 10:07:10 +0530

    Added internet gateway and route table

--- networks.tf
+++ networks.tf
@@ -1,3 +1,4 @@
+// virtual private cloud
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -7,6 +8,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+// subnet for frontend resources
 resource "aws_subnet" "frontend" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/18"
@@ -16,6 +18,7 @@ resource "aws_subnet" "frontend" {
   }
 }
 
+// subnet for backend resources
 resource "aws_subnet" "backend" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.64.0/18"
@@ -25,6 +28,7 @@ resource "aws_subnet" "backend" {
   }
 }
 
+// subnet for database resources
 resource "aws_subnet" "database" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.128.0/18"
@@ -33,5 +37,36 @@ resource "aws_subnet" "database" {
     Name = "database"
   }
 }
+
+// internet gateway for the VPC
+resource "aws_internet_gateway" "main_internet_gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_internet_gateway"
+  }
+}
+
+// route table for the public subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_internet_gateway.id
+  }
+
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+// route table association for the frontend subnet
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.frontend.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
 
 
