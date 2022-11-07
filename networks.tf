f6d750ba856125da049c443cba721fd8b9f7664d
Krishan Shamod <krishanshamod@gmail.com>
2022-11-07 11:31:04 +0530

    Added route table for the private subnets

--- networks.tf
+++ networks.tf
@@ -62,7 +62,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 // route table association for the frontend subnet
-resource "aws_route_table_association" "public_subnet_association" {
+resource "aws_route_table_association" "frontend_subnet_association" {
   subnet_id      = aws_subnet.frontend.id
   route_table_id = aws_route_table.public_route_table.id
 }
@@ -87,5 +87,32 @@ resource "aws_nat_gateway" "main_nat_gateway" {
 
   depends_on = [aws_internet_gateway.main_internet_gateway]
 }
+
+// route table for the private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main_nat_gateway.id
+  }
+
+  tags = {
+    Name = "private_route_table"
+  }
+}
+
+// route table association for the backend subnet
+resource "aws_route_table_association" "backend_subnet_association" {
+  subnet_id      = aws_subnet.backend.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+// route table association for the database subnet
+resource "aws_route_table_association" "database_subnet_association" {
+  subnet_id      = aws_subnet.database.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 
 
